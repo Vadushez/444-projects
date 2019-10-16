731c090221344f7d2816aa9f65833ffb5ed2ad84
Vadushez <kopalka1998@gmail.com>
2019-10-16 11:04:31 +0300

    Tasks

--- task3/main.cpp
+++ task3/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    double a,b,c;
+    cout<<"Vvedite poocheredno koefficenti kvadratnogo uravneniya"<<endl;
+    cin>>a>>b>>c;
+    double D=b*b-4*a*c;
+    if (D<0)
+    {
+    cout<<"Resheniy net";
+    }
+    if (D==0)
+    {
+        cout<<"1 reshenie"<<" ";
+        cout<<"{"<<-b/(2*a)<<"}";
+    }
+    if (D>0)
+    {
+    cout<<"2 Resheniya"<<" ";
+    int x1=(-b+sqrt(D))/(2*a);
+    int x2=(-b-sqrt(D))/(2*a);
+    cout<<"{"<<x1<<","<<x2<<"}";
+    }
+
+
 }
